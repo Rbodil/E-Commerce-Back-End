4922aa53d268cd082d0df365b0fe5993c11052f8
Reagan Bodily <reaganbodily@gmail.com>
2022-04-19 21:59:14 -0600

    built models

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,16 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id:{
+      type: Datatypes.INTEGER,
+      autoIncrement: true,
+      primaryKey:true,
+      allowNull:false
+    },
+    category_name:{
+      type: Datatypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
